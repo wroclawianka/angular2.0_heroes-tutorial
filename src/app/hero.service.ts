8cd8c15c0317fd4ca27d1dc2a8296ccbd4b52029
wroclawianka <dorota.zelga@gmail.com>
2017-12-03 12:21:51 +0100

    delete hero enabled

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -59,6 +59,15 @@ export class HeroService {
       );
   }
 
+  deleteHero(hero : Hero | number) : Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id; //conditional (ternary) operation 
+    return this.http.delete<Hero>(this.heroUrl(id), httpOptions)
+    .pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
   }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -30,4 +30,12 @@ export class HeroesComponent implements OnInit {
         this.heroes.push(hero);
       })
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+    /*If you neglect to subscribe(), the service will not send 
+    the delete request to the server! As a rule, 
+    an Observable does nothing until something subscribes! */
+  }
 }
